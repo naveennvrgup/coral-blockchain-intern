44e35310458dc126a58fb2c181d9a75f64d469c8
naveennvrgup <naveennvrgup@gmail.com>
2019-04-21 00:30:34 +0530

    final

--- golang-back/main.go
+++ golang-back/main.go
@@ -13,19 +13,22 @@ import (
 )
 
 type userData struct {
-	gorm.Model
+	// gorm.Model
 	UserName string `json:"userName" gorm:"varchar(25);not null"`
-	EmailId string	`json:"emailId" gorm:"varchar(50);not null"`
+	EmailId string	`json:"emailId" gorm:"varchar(50);not null;primary_key"`
 	PhoneNo string `json:"phoneNo" gorm:"varchar(10);not null"`
 	Password string `json:"password" gorm:"varchar(50);not null"`
-	Datetime time.Time `json:"datetime"`
+	Datetime time.Time `json:"dateTime"`
 }
 
 func (userData) TableName() string {
-	return "userData"
+	return "naveenuserData"
 }
+// var sequelize = new Sequelize('db_intern', 'dummyUser', 'dummyUser01', {
+// 	host: 'db-intern.ciupl0p5utwk.us-east-1.rds.amazonaws.com',
 
-var uri string= "newuser:password@/test?charset=utf8&parseTime=True&loc=Local"
+// var uri string= "newuser:password@/test?charset=utf8&parseTime=True&loc=Local"
+var uri string= "dummyUser:dummyUser01@tcp(db-intern.ciupl0p5utwk.us-east-1.rds.amazonaws.com:3306)/db_intern"
 
 func findUser(w http.ResponseWriter, r *http.Request) {
 	db, err := gorm.Open("mysql", uri)
@@ -63,6 +66,23 @@ func addUser(w http.ResponseWriter, r *http.Request) {
         panic(err)
 	}
 
+	// check for existing record
+	var users []userData
+	db.Where("email_id = ?", body["emailId"]).Find(&users)
+	if len(users)>0 {
+		user :=users[0]
+		user.UserName = body["userName"].(string)
+		user.EmailId = body["emailId"].(string)
+		user.Password = body["password"].(string)
+		user.PhoneNo = body["phoneNo"].(string)
+		user.Datetime = time.Now()
+		db.Save(&user)
+
+		result := map[string]string{"msg":"updated " + body["emailId"].(string) + " successfully"} 
+		json.NewEncoder(w).Encode(result)
+		return
+	}
+
 	db.Create(&userData{
 		UserName: body["userName"].(string), 
 		EmailId: body["emailId"].(string),
@@ -70,7 +90,9 @@ func addUser(w http.ResponseWriter, r *http.Request) {
 		PhoneNo: body["phoneNo"].(string),
 		Datetime: time.Now(),
 	})
-	fmt.Fprintf(w, "User " + body["emailId"].(string) + " has be created successfully")
+
+	result := map[string]string{"msg":"User " + body["emailId"].(string) + " has be created successfully"} 
+	json.NewEncoder(w).Encode(result)
 }
 
 func deleteUser(w http.ResponseWriter, r *http.Request) {
@@ -89,26 +111,6 @@ func deleteUser(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "Successfully Deleted User")
 }
-
-// func updateUser(w http.ResponseWriter, r *http.Request) {
-// 	db, err := gorm.Open("mysql", uri)
-// 	if err != nil {
-// 		panic("failed to connect database")
-// 	}
-// 	defer db.Close()
-
-// 	vars := mux.Vars(r)
-// 	name := vars["name"]
-// 	email := vars["email"]
-
-// 	var user User
-// 	db.Where("name = ?", name).Find(&user)
-
-// 	user.Email = email
-
-// 	db.Save(&user)
-// 	fmt.Fprintf(w, "Successfully Updated User")
-// }
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
@@ -138,7 +140,7 @@ func initialMigration() {
 
 func main() {
 	log.Println("gorilla upon 3000")
-	
+
 	initialMigration()
 	// Handle Subsequent requests
 	handleRequests()
